1b00c364238ee724f38c157c4fe1048250f6afc4
ROMY2004 <ibrahimromia101@gmail.com>
2023-11-21 22:22:00 +0200

    Start Point

--- WATCH IT APP/src/main/java/com/watchit/watch_it_app/HelloApplication.java
+++ WATCH IT APP/src/main/java/com/watchit/watch_it_app/HelloApplication.java
@@ -3,8 +3,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
--- WATCH IT APP/src/main/java/com/watchit/watch_it_app/HelloController.java
+++ WATCH IT APP/src/main/java/com/watchit/watch_it_app/HelloController.java
@@ -2,6 +2,7 @@ package com.watchit.watch_it_app;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+
 public class HelloController {
     @FXML
     private Label welcomeText;
