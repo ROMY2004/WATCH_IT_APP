d6c5858144eab88c5b12c8eb1a19a642c60980cb
ROMY <141304132+ROMY2004@users.noreply.github.com>
2023-11-21 22:19:59 +0200

    Delete WATCH IT APP directory

--- WATCH IT APP/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.watchit.watch_it_app {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.watchit.watch_it_app to javafx.fxml;
-    exports com.watchit.watch_it_app;
-}
