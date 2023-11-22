52fbd6acd8d44565036c025a82cf892d5d228395
ROMY <141304132+ROMY2004@users.noreply.github.com>
2023-11-22 14:25:21 +0200

    Delete WATCH AND CHILL directory

--- WATCH AND CHILL/src/main/java/com/watchit/watch_and_chill/HelloController.java
+++
@@ -1,8 +0,0 @@
-package com.watchit.watch_and_chill;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-public class HelloController {
-
-}
--- WATCH AND CHILL/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.watchit.watch_and_chill {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.watchit.watch_and_chill to javafx.fxml;
-    exports com.watchit.watch_and_chill;
-}
